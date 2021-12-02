f32ddb10640e80589e2668a9cb55d9759ccb2d3b
Nestor Plasencia <nplasencia82@gmail.com>
2021-12-02 16:25:00 +0000

    [ADD] Advent of code - day 2

---
+++ advent-of-code-2021/Day 2/part2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s_temp;
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+    while(cin) {
+        if( cin.eof() ) break;
+        getline(cin,s_temp);
+        string dir_str;
+        string value_str;
+        int count = 0;
+        int p = 0;
+        for(int i = 0; i < s_temp.length(); i++) {
+            char l = s_temp[i];
+            if(l == ' '){
+                count++;
+            }else{
+                if(count == 0){
+                    dir_str += l;
+                }
+                if(count == 1){
+                    value_str += l;
+                }
+            }
+        }
+        if( dir_str == "forward") {
+            horizontal += stoi(value_str);
+            depth += aim*stoi(value_str);
+        }
+        if( dir_str == "down") {
+            aim += stoi(value_str);
+        }
+        if( dir_str == "up") {
+            aim -= stoi(value_str);
+        }
+    };
+    cout<<horizontal*depth<<endl;
+    return 0;
+}
